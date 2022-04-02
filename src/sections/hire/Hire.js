b49a10241661f060b0bebdfdcb32e75b9db4d5fe
joseph-p-otoole <otooleJoseph@outlook.com>
2022-04-02 13:40:29 -0500

    icons and images

--- src/sections/hire/Hire.js
+++ src/sections/hire/Hire.js
@@ -3,26 +3,42 @@ import React from 'react';
 import './Hire.css';
 import Link from '../../components/nav/Link';
 import Cog from '../../assets/icons/cog.svg';
+import BrowserIcon from '../../assets/icons/browser.png';
+import LinkIcon from '../../assets/icons/link.png';
+import LayersIcon from '../../assets/icons/layers.png';
+import BrushIcon from '../../assets/icons/brush.png';
 
 const Hire = () => {
 	return (
 		<div id='hire'>
 			<div className='contentContainer'>
 				<div className="textContainer">
-					<h4><span className="oi oi-browser"></span><br/>Front End</h4>
-					<p>I am passionate about front end development. I continually look for ways to improve the flexibility and athstetics of my work and I love bringing a page to life with animations.</p>
+					<div className='decoIconWrapper'>
+						<img className='decoIcon' src={BrowserIcon} alt='browser icon'/>
+					</div>
+					<h4 className='hireHeadline'>Front End</h4>
+					<p className='hireText'>I am passionate about front end development. I continually look for ways to improve the flexibility and athstetics of my work and I love bringing a page to life with animations.</p>
 				</div>
 				<div className="textContainer">
-					<h4><span className="oi oi-link-intact"></span><br/>Back End</h4>
-					<p>I am comfortable with back end development. I enjoy the challenge of designing an efficient database and find satisfaction in the execution of a plan.</p>
+					<div className='decoIconWrapper'>
+						<img className='decoIcon' src={LinkIcon} alt='link icon'/>
+					</div>
+					<h4 className='hireHeadline'>Back End</h4>
+					<p className='hireText'>I am comfortable with back end development. I enjoy the challenge of designing an efficient database and find satisfaction in the execution of a plan.</p>
 				</div>
 				<div className="textContainer">
-					<h4><span className="oi oi-layers"></span><br/>UI/UX</h4>
-					<p>I am fascinated with the psychology of UI design. I strive to create intuitive, elegant designs and aim to provide users with a comfortable yet unique experience.</p>
+					<div className='decoIconWrapper'>
+						<img className='decoIcon' src={LayersIcon} alt='layers icon'/>
+					</div>
+					<h4 className='hireHeadline'>UI/UX</h4>
+					<p className='hireText'>I am fascinated with the psychology of UI design. I strive to create intuitive, elegant designs and aim to provide users with a comfortable yet unique experience.</p>
 				</div>
 				<div className="textContainer">
-					<h4><span className="oi oi-brush"></span><br/>Design</h4>
-					<p>I love creating and working with vector graphics. I thoroughly enjoy the process of creating every logo, icon and graphic I make. </p>
+					<div className='decoIconWrapper'>
+						<img className='decoIcon' src={BrushIcon} alt='brush icon'/>
+					</div>
+					<h4 className='hireHeadline'>Design</h4>
+					<p className='hireText'>I love creating and working with vector graphics. I thoroughly enjoy the process of creating every logo, icon and graphic I make. </p>
 				</div>
 			</div>
 			<Link icon={Cog} type='actionLink link' reference='#about' name='About' />
--- src/sections/skills/Skills.js
+++ src/sections/skills/Skills.js
@@ -210,4 +210,4 @@ const Skills = (props) => {
 	)
 }
 
-export default Skills
+export default Skills;
